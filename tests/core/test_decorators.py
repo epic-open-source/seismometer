f8f9693bda57d6fab160f9a7b2bb533d8051e202
Garry Bowlin <gsbowlin@gmail.com>
2024-06-21 13:59:50 +0000

    add tests to handle middle rows alteratiosn to large dataframes

--- tests/core/test_decorators.py
+++ tests/core/test_decorators.py
@@ -1,6 +1,8 @@
 from io import StringIO
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
 import pytest
 
 from seismometer.core.decorators import DiskCachedFunction, export
@@ -79,6 +81,34 @@ def disk_cached_str(tmp_path):
     cache_decorator.enable()
     yield cache_decorator
     cache_decorator.clear_all()
+
+
+@pytest.fixture
+def large_dataframes():
+    """two dataframes with the same data at the beginning and end, but different in the middle"""
+    size = 2000
+    df1 = pd.DataFrame(
+        {
+            "Val": np.random.normal(20, 10, size=size),
+            "Cat": np.random.choice(["A", "B", "C", "D"], size=size, replace=True, p=[2 / 5, 1 / 2, 3 / 40, 1 / 40]),
+            "T/F": np.random.choice([0, 1, np.nan], size=size, replace=True, p=[4 / 7, 2 / 7, 1 / 7]),
+        }
+    )
+
+    # make the last row the different from the first
+    df1.at[1999, "Val"] = -df1.at[0, "Val"]
+    df1.at[1999, "Cat"] = "A" if df1.at[0, "Cat"] == "B" else "B"
+    df1.at[1999, "T/F"] = 1 if df1.at[0, "T/F"] == 0 else 0
+    df2 = df1.copy(deep=True)  # copy the first dataframe
+
+    # replace the middle third of the second dataframe with new data
+    start, stop = size // 3, 2 * size // 3
+    middle_rows = df1[start:stop].copy(deep=True)
+    middle_rows["Val"] = -middle_rows["Val"]
+    middle_rows["Cat"] = middle_rows["Cat"].apply(lambda x: "A" if x == "B" else "B")
+    middle_rows["T/F"] = middle_rows["T/F"].apply(lambda x: 1 if x == 0 else 0)
+    df2[start:stop] = middle_rows
+    return df1, df2
 
 
 class Test_DiskCachedFunction:
@@ -169,8 +199,6 @@ class Test_DiskCachedFunction:
 
 class Test_DiskCachedFunction_With_Pandas:
     def test_supports_dataframe_cachable(self, disk_cached_str):
-        import pandas as pd
-
         global count
         count = 0
 
@@ -189,8 +217,6 @@ class Test_DiskCachedFunction_With_Pandas:
         assert count == 1
 
     def test_supports_dataframe_reordered_as_cachable(self, disk_cached_str):
-        import pandas as pd
-
         global count
         count = 0
 
@@ -210,8 +236,6 @@ class Test_DiskCachedFunction_With_Pandas:
         assert count == 1
 
     def test_supports_dataframe_can_skip_cachable(self, disk_cached_str):
-        import pandas as pd
-
         global count
         count = 0
 
@@ -229,3 +253,59 @@ class Test_DiskCachedFunction_With_Pandas:
         assert count == 1
         assert foo(df) == ",a,b\n0,1,4\n1,2,5\n2,3,6\n"
         assert count == 2
+
+    def test_can_differentiate_similar_dataframes(self, disk_cached_str, large_dataframes):
+        """
+        This test is to assure that if we swap some of the data that is not visible to the
+        stringified dataframe (which includes start and end rows only), that the cache will
+        still be able to differentiate between the two dataframes, via a valid hash
+        """
+        global count
+        count = 0
+
+        @disk_cached_str
+        def foo(x: pd.DataFrame) -> str:
+            global count
+            count += 1
+            # return the middle row as a sttring
+            return str(x.iloc[len(x) // 2])
+
+        df1, df2 = large_dataframes
+        mid1 = str(df1.iloc[len(df1) // 2])
+        mid2 = str(df2.iloc[len(df2) // 2])
+
+        assert str(df1) == str(df2)  # fixture used in test should make sure that these are the same
+
+        assert foo(df1) == mid1
+        assert count == 1
+        assert foo(df2) == mid2  # different data
+        assert count == 2
+
+    def test_can_match_large_dataframes(self, disk_cached_str, large_dataframes):
+        """
+        This test is to assure that if we reorder some of the data, that the cache will
+        still be able to match two dataframes with the same content, via a valid hash
+        """
+        global count
+        count = 0
+
+        @disk_cached_str
+        def foo(x: pd.DataFrame) -> str:
+            global count
+            count += 1
+            # return the first row as a sttring
+            x = str(x.iloc[0])
+            return x
+
+        df1, _ = large_dataframes
+        df2 = df1.sort_index(ascending=False).sort_index(axis=1, ascending=True)  # reverse rows, resort columns
+        first1 = str(df1.iloc[0])
+        first2 = str(df2.iloc[0])
+        assert first1 != first2  # fixture used in test should make sure that these are different
+
+        assert foo(df1) == first1
+        assert count == 1
+
+        assert foo(df2) == first1
+        assert foo(df2) != first2
+        assert count == 1
