bceab381527a96ca1f756af1a67432d20c9aad55
metedadi <metedadi@epic.com>
2024-12-09 22:50:29 +0000

    fix table alignments

--- src/seismometer/table/analytics_table_config.py
+++ src/seismometer/table/analytics_table_config.py
@@ -20,7 +20,7 @@ class AnalyticsTableConfig:
     ):
         self.decimals = decimals
         self.columns_show_percentages = columns_show_percentages
-        self.columns_show_bar = columns_show_bar
+        self.columns_show_bar = columns_show_bar or {}
 
         self.percentages_decimals = percentages_decimals
         self.alternating_row_colors = alternating_row_colors
@@ -51,17 +51,17 @@ class AnalyticsTableConfig:
 
     @columns_show_bar.setter
     def columns_show_bar(self, value):
-        self._columns_show_bar = value if value else {"AUROC": "lightblue", "PPV": "lightgreen"}
+        self._columns_show_bar = value
         # If a provided column name is one of the columns generated here (in particular, df is not None),
         # then we allow the column name to be case insensitive.
-        self._columns_show_bar = {
-            GENERATED_COLUMNS.get(col.lower(), col): self._columns_show_bar[col] for col in self._columns_show_bar
-        }
+        if self._columns_show_bar:
+            self._columns_show_bar = {
+                GENERATED_COLUMNS.get(col.lower(), col): self._columns_show_bar[col] for col in self._columns_show_bar
+            }
 
 
 COLORING_CONFIG_DEFAULT = {
     "decimals": 3,
     "columns_show_percentages": ["Prevalence"],
-    "columns_show_bar": {"AUROC": "lightblue", "PPV": "lightblue"},
     "percentages_decimals": 2,
 }
