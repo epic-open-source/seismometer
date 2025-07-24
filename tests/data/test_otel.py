d0a0a90ad11200ec4ad8d15d1daaf5392510396f
Adam Hutchings <ahutchin@epic.com>
2025-07-24 21:09:19 +0000

    Add tests for basic export manager functionality

---
+++ tests/data/test_otel.py
@@ -0,0 +1,22 @@
+import pytest
+
+from seismometer.data.otel import RealExportManager
+
+
+class TestExportManager:
+    def test_unusable_create(self):
+        """Creating an export manager with nowhere to export should error."""
+        with pytest.raises(Exception):
+            # Force use to actually make a new class instance.
+            RealExportManager.kill()
+            _ = RealExportManager()
+
+    def test_class_activate(self):
+        r = RealExportManager(dump_to_stdout=True)
+        r.activate_exports()
+        assert r.active
+
+    def test_class_deactivate(self):
+        r = RealExportManager(dump_to_stdout=True)
+        r.deactivate_exports()
+        assert not r.active
