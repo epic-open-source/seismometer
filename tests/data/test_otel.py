0734d1bf624695ae1c29f84167fa1d1201c05a22
Adam Hutchings <ahutchin@epic.com>
2025-07-29 18:12:10 +0000

    Mock out all OTel dependencies

--- tests/data/test_otel.py
+++ tests/data/test_otel.py
@@ -1,16 +1,17 @@
-import pytest
+from unittest.mock import MagicMock, patch
 
 from seismometer.data.otel import RealExportManager
 
 
+@patch("seismometer.data.otel.Resource", new=MagicMock())
+@patch("seismometer.data.otel.SERVICE_NAME", new="mocked_service")
+@patch("seismometer.data.otel.PeriodicExportingMetricReader", new=MagicMock())
+@patch("seismometer.data.otel.ConsoleMetricExporter", new=MagicMock())
+@patch("seismometer.data.otel.MeterProvider", new=MagicMock())
+@patch("seismometer.data.otel.set_meter_provider", new=MagicMock())
+@patch("seismometer.data.otel.Meter", new=MagicMock())
+@patch("seismometer.data.otel.OTLPMetricExporter", new=MagicMock())
 class TestExportManager:
-    def test_unusable_create(self):
-        """Creating an export manager with nowhere to export should error."""
-        with pytest.raises(Exception):
-            # Force use to actually make a new class instance.
-            RealExportManager.kill()
-            _ = RealExportManager()
-
     def test_class_activate(self):
         r = RealExportManager(hostname="", file_output_paths=[], export_ports=[], dump_to_stdout=True)
         r.activate_exports()
